8f1cd2553eb7f7501f3f5c7f5e398c07ada9afc7
vyas-shubham <43845118+vyas-shubham@users.noreply.github.com>
2025-08-04 08:21:52 +0000

    Deploying to gh-pages from @ vyas-shubham/vyas-shubham.github.io@153679c0ac045ef46702888312d53479a544c988 🚀

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     mode_toggle.addEventListener("click", function() {
         toggleTheme(localStorage.getItem("theme"));
+        if (typeof window.renderChart === "function") window.renderChart();
     });
 });
 
